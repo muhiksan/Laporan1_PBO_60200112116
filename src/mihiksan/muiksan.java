f296a3dae0203b84140612f366c7835df8f38262
muhiksan <60200112116@uin-alauddin.ac.id>
2015-04-22 15:54:16 +0800

    versi 1.0.o

---
+++ src/mihiksan/muiksan.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package mihiksan;
+
+/**
+ * @author acer
+ *
+ */
+public class muiksan {
+
+	/**
+	 * @param args
+	 */
+	static String pengetahuan1 = "Print";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String praktikum1 = "BIODATA";
+		System.out.println(praktikum1);
+		biodata();
+		pembagian();
+
+	}
+	
+	public static void biodata()
+	{
+		System.out.println("muhiksan");
+		System.out.println("60200112116");
+		System.out.println("KelasC");
+	}
+	
+	public static void pembagian()
+	{
+		float a = 10/2;
+		float b=6/8;
+		System.out.println("PEMBAGIAN");
+		System.out.println("Hasilnya Adalah:"+" "+a);
+		System.out.println("Hasilnya Adalah:"+" "+b);
+	}
+
+}
